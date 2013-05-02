0ceda8041f90d70f7dee396f70db61d28e166ecd
Jakub Dundalek <dundalek@gmail.com>
2013-05-02 20:47:14 +0200

    add api contents

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@
 for f in */; do cd "$f"; [ -e build.js ] && echo "$f" && node build.js; cd ..; done
 
 echo -e "Title: KMDoc Knowledge Base\nHTML header: <link rel=\"stylesheet\" type=\"text/css\" href=\"components/bootstrap/css/bootstrap.min.css\">\n" | cat - README.md | multimarkdown | sed "s/<body>/<body>\n<div class=\"container\">/;s/<\\/body>/<\\/div>\n<\\/body>/" > index.html
+
+rm -rf api
+cp -r /home/joudy/Dropbox/github/kmdoc/kb/components/kmdoc/doc/api .
