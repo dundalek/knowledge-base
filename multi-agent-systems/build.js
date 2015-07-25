b659dd2f7d6e03a938489e0e572f079c7e93c1b6
Jakub Dundalek <dundalek@gmail.com>
2015-07-25 18:41:27 +0200

    enable mindmap for multi-agent-systems

--- multi-agent-systems/build.js
+++ multi-agent-systems/build.js
@@ -2,6 +2,6 @@ var kmd = require('../kmdoc.common').create({
     title: 'Multi-Agent Systems'
 });
 
-kmd.use('shortsource', 'toc', 'tooltip', 'math', 'recall', 'autolink', 'search', 'flashcard');
+kmd.use({'mindmap': {autoOpen: true, out: 'index-mindmap.json'}}, 'shortsource', 'toc', 'tooltip', 'math', 'recall', 'autolink', 'search', 'flashcard');
 
 kmd.build();
